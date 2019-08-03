9c168140084673ba3c048fa274629147268edbb7
Milobo <dinhminhx3@gmail.com>
2019-08-03 10:57:00 +0700

    Update ViewPager.js

--- viewpager/ViewPager.js
+++ viewpager/ViewPager.js
@@ -6,7 +6,7 @@
 
 import { PanResponder, Platform, ScrollView, StyleSheet, View } from 'react-native'
 import React, { Component } from 'react'
-imported ViewPagerAndroid from '@react-native-community/viewpager'
+import ViewPagerAndroid from '@react-native-community/viewpager'
 
 const SCROLLVIEW_REF = 'scrollView'
 const VIEWPAGER_REF = 'viewPager'
